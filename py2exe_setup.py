bed4381502ad9b000de06b2ade48d2c3d4ac3f94
cum~ <hentai@localhost>
2012-05-01 14:58:45 +0100

    amend setup

--- py2exe_setup.py
+++ py2exe_setup.py
@@ -1,22 +1,28 @@
-# build the exe with this script
 # usage: 
 # - copy msvcp90.dll in this directory (it's in the exe dir)
-# - navigate to the directory in command line
 # - python py2exe_setup.py py2exe
-# - the dist directory contains the exe (and everything necessary)
 
 from distutils.core import setup
 import py2exe
 
-data_files = ['MSVCP90.dll']
+data_files = [
+    'MSVCP90.dll',
+    'fg.py',
+    'setgifdelay.py',
+    'py2exe_setup.py',
+    'README.txt',
+    'BUGS.txt',
+    'CHANGELOG.txt',
+]
 setup(
         options={
-            "py2exe":{
-                #"bundle_files":2,
-                "optimize":2,
-                "dist_dir":"dist_9",
-                "compressed":True,
+            "py2exe": {
+                "bundle_files": 2,
+                "optimize": 2,
+                "dist_dir": "fg-8",
+                "compressed": True,
             }
         },
         console=['fg.py'], data_files=data_files
 )
+
